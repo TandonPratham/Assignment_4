fa99f4eeb46a5e2b0696c4508c6204e7939bb25d
TandonPratham <117387692+TandonPratham@users.noreply.github.com>
2022-12-03 14:05:03 +0530

    Update _1_Add_25_number.py

--- _1_Add_25_number.py
+++ _1_Add_25_number.py
@@ -1,3 +1,3 @@
 #  If want to add another number please change the value 
 result= lambda  number :number + 25
-print("After adding 10 to 25 result will be = ",result(10))
+print("After adding number to 25 result will be = ",result(10))
